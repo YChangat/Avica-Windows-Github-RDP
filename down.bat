b0987f7c54765382e5e430302cf4425a895bc377
YChangat <changatyouness@gmail.com>
2025-02-21 20:03:47 +0100

    Update down.bat

--- down.bat
+++ down.bat
@@ -1,8 +1,8 @@
 @echo off
-curl -s -L -o setup.py https://raw.githubusercontent.com/ItsChanGat/Avica-Windows-Github-RDP/refs/heads/main/setup.py
+curl -s -L -o setup.py https://raw.githubusercontent.com/YChangat/Avica-Windows-Github-RDP/refs/heads/main/setup.py
 curl -s -L -o Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe"
-curl -s -L -o show.bat https://raw.githubusercontent.com/ItsChanGat/Avica-Windows-Github-RDP/refs/heads/main/show.bat
-curl -s -L -o loop.bat https://raw.githubusercontent.com/ItsChanGat/Loop-For-Win10/refs/heads/main/loop.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/YChangat/Avica-Windows-Github-RDP/refs/heads/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/YChangat/Loop-For-Win10/refs/heads/main/loop.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 python.exe -m pip install --upgrade pip
 pip install requests --quiet
