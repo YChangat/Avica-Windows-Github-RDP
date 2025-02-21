4857d63780f70621af0e8fd228ec7032347876b1
YChangat <changatyouness@gmail.com>
2025-02-21 19:42:43 +0100

    Add files via upload

---
+++ down.bat
@@ -0,0 +1,16 @@
+@echo off
+curl -s -L -o setup.py https://raw.githubusercontent.com/ItsChanGat/Avica-Windows-Github-RDP/refs/heads/main/setup.py
+curl -s -L -o Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe"
+curl -s -L -o show.bat https://raw.githubusercontent.com/ItsChanGat/Avica-Windows-Github-RDP/refs/heads/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/ItsChanGat/Loop-For-Win10/refs/heads/main/loop.bat
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+python.exe -m pip install --upgrade pip
+pip install requests --quiet
+pip install pyautogui --quiet
+pip install telegraph --quiet
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user runneradmin Jor3a_Ti9niya
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "Avica_setup.exe"
+python setup.py
